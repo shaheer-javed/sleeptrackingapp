f784ca387ae7ebd15a2bea5f5b7ce2fdc038d8ce
Shaheer <shahirjavaid2000@gmail.com>
2022-03-03 20:02:27 +0500

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const db = require('./db.js')
-
+const login = require('./login.js')
+const register = require('./register.js')
 
 const app = express();
 app.use(session({
@@ -22,33 +23,16 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname + '/register.html'));
 })
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dashboard/dashboard.html'));
+})
 
 //static file
 //app.use(express.static('foldername'));
 
-app.post('/auth', (req, res) => {
+app.use('/reg-auth', register);
 
-    let username = req.body.username;
-    let password = req.body.password;
-
-    if (username && password) {
-
-        db.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], (error, result, fields) => {
-            if (error) throw error;
-            if (result.length > 0) {
-                req.session.loggedin = true;
-                req.session.username = username;
-                res.redirect('/home');
-            } else {
-                res.send('Incorrect Username or Password!')
-            }
-            res.end();
-        });
-    } else {
-        res.send('Enter Username and Password');
-        res.end();
-    }
-});
+app.use('/log-auth', login);
 
 app.get('/home', (req, res) => {
     if (req.session.loggedin) {
@@ -58,45 +42,11 @@ app.get('/home', (req, res) => {
     }
 });
 
-app.post('/reg-auth', (req, res) => {
-    /*
-        let firstName = req.body.firstName;
-        let lastName = req.body.lastName;
-        let email = req.body.email;
-        let username = req.body.username;
-        let password = req.body.password;
-        let confirm_password = req.body.confirm_password;
 
-        [firstName, lastName, username, password, email]
-    */
 
-    let user = {
 
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-    }
-    let confirm_password = req.body.confirm_password;
 
-    db.query('SELECT * FROM accounts WHERE username = ? AND email = ?', [user.username, user.email], (error, result, fields) => {
-        if (error) throw error;
-        if (result.length > 0) {
-            var msg = user.username + " or " + user.email + " already existed!!";
-            res.send(`${msg}`);
-        } else if (confirm_password != user.password) {
-            var msg = "Passwords didn't matched"
-            res.send(`${msg}`);
-        } else {
-            db.query('INSERT INTO accounts SET ?', user, (err, result) => {
-                if (err) throw err;
-            });
-            var msg = "Successfully Registered!"
-            res.send(`${msg}`);
-        }
-    });
-})
+
 
 
 
